e6e58a8be584c3d2e9a6e3b4a1d9b60cdbc969b4
Chi Sxing <66873237+SxingBmo@users.noreply.github.com>
2025-09-10 09:58:37 +0700

    Update web build files

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"dd93de6fb1776398bf586cbd477deade1391c7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1992449072"
+    serviceWorkerVersion: "3637745030"
   }
 });
